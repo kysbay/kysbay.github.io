e09dd718f6e02ab1fa3fbb73c58cfb818fc2d137
Pavan Kumar Yekabote <yekabotep@gmail.com>
2023-11-05 15:50:26 +0530

    Feat: added main section, the landing page

---
+++ src/app/components/main-section/main-section.component.ts
@@ -0,0 +1,14 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-main-section',
+    templateUrl: './main-section.component.html',
+    styleUrls: ['./main-section.component.scss'],
+})
+export class MainSectionComponent implements OnInit, AfterViewInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    ngAfterViewInit(): void {}
+}
